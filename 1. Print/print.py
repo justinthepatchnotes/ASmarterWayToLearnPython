d6a46cec99579b639b25e57a3b6cbeac1f2643df
Justin J. <justinthepatchnotes@gmail.com>
2025-01-05 09:16:02 -0500

    Create print.py

---
+++ 1. Print/print.py
@@ -0,0 +1,46 @@
+# What keyword tells Python to display something on the screen?
+
+print 
+
+# Complete the line of code to tell Python to display the name of the oval object laid by a hen. (1 word, lower-case)
+
+print ("egg")
+
+# Tell Python to display the letter "e".
+
+print ("e")
+
+# Tell Python to display the name of our planet. (1 word, lower-case)
+
+print ("earth")
+
+# Tell Python to display the name of the star that makes life possible on earth. (1 word, lower-case)
+
+print ("sun")
+
+# In a print statement, the characters surrounded by quotation marks are a text _______.
+
+# string
+
+# Tell Python to display the first three letters of the alphabet in upper-case.
+
+print("ABC")
+
+# Tell Python to display a string of your choice
+
+print("patch notes")
+
+# Tell Python to display your first name.
+print("Justin")
+
+# Tell Python to display the three-letter word for a canine. (1 word, lower-case)
+
+print("dog")
+
+# write a statement telling Python to display the string "Hello, World!"
+
+print("Hello World!")
+
+# write a statement telling Python to display your first name. 
+
+print("Justin")
