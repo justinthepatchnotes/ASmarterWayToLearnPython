c3fb6151d0575d44d77a432edede1812870ab48d
Justin J. <justinthepatchnotes@gmail.com>
2025-01-14 04:48:22 -0500

    Update test.py

--- 0. Test/test.py
+++ 0. Test/test.py
@@ -1,4 +1,4 @@
-=/= ==> <== +=> :== 
+=/= ==> <== +=> === ==/== /> </ 
 
 
 print
@@ -8,3 +8,22 @@ if
 while
 for 
 class 
+
+import pandas as pd
+
+def groundbreaking_stuff(data: pd.DataFrame, breaking: bool) -> pd.DataFrame:
+    """
+    Groundbreaking stuff done to the data
+    Args:
+        data (pd.DataFrame): Input Data
+    Returns:
+        pd.DataFrame: Output Data
+    """
+    grounded_data = data.ground()
+
+    if breaking:
+        return grounded_data.breaking()
+
+    return grounded_data
+ 
+ 
