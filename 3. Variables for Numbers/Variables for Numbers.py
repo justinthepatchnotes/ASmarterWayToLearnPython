27a61efd39592b00c8b4568c4dc8ec64eb5ca1e1
Justin J. <justinthepatchnotes@gmail.com>
2025-01-14 13:55:49 -0500

    Create Variables for Numbers.py

---
+++ 3. Variables for Numbers/Variables for Numbers.py
@@ -0,0 +1,71 @@
+# There are two correct answers for this one. Answer with either one. What is this?
+
+100
+
+# a number or a 
+
+# Type the illegal character in this variable name.
+
+nd_runner_up = 1
+
+# Variable names in most programming languages cannot start with a digit; they must begin with a letter or an underscore (_). After the first character, digits, letters, and underscores are usually allowed.
+
+# Assign the number 5 to the variable players
+
+players = 5
+
+# Assign the negative number -6 to the variable loss
+
+loss = -6
+
+# What is the value of total?
+
+number = 10
+total = number + 2
+
+print(total)
+
+# Assign a number (your choice) to the variable total.
+
+total = 313
+
+# Assign a negative integer to a variable. You choose the integer and the variable name.
+
+name = -313
+
+# What is this (one word, lowercase)?
+
+"-8.087"
+
+# string
+
+# Add two variables and assign the result to a third variable. Make up all the variable names.
+
+a = 1
+b = 2
+c = a + b
+
+print(c)
+
+# Add 5 to the variable running_total and assign the result to the same variable
+
+running_total = 5
+running_total = running_total + 5
+
+# assign the sum of two numbers to a variable. Make up the variable name and the numbers.tell Python to display the sum using the variable.
+
+z = 1
+x =2
+
+c = z + x
+
+print(c) 
+
+total = 102.20
+
+# Divide the variable by a number of your choice. Assign the result to a variable of your choice. On line 3, tell Python to display the result using that second variable
+
+total / 2
+
+print(total)
+
