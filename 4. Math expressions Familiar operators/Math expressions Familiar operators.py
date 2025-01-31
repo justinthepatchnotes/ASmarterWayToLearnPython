1a915d717a0c10346ed85b9a2c4004c6ce0794b5
Justin J. <justinthepatchnotes@gmail.com>
2025-01-31 00:57:24 -0500

    Create Math expressions Familiar operators.py

---
+++ 4. Math expressions Familiar operators/Math expressions Familiar operators.py
@@ -0,0 +1,76 @@
+# Rewrite this so total equals 10.
+
+total = "4" + 6
+
+total = 4 + 6 
+print(total)
+
+# What is the value of third_number?
+
+first_number = 5
+second_number = 6
+third_number = first_number * second_number
+
+print(third_number)
+# 30
+
+# Divide 12 by the decimal .5 and assign the result to the variable outcome
+
+outcome = 12 / .5
+print(outcome)
+
+# Divide the variable total by 3 and assign the result to the variable each_share.
+total = 1
+
+each_share = total / 3
+print(each_share)
+
+
+# Subtract one number from another and assign the result to a variable. Make up the numbers and the variable name.
+
+total_cost = 100 - 5
+print(total_cost)
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+
+time = 1
+rate = 2
+
+hour = time / rate 
+print (hour)
+
+# Subtract a variable from a number and assign the result to a second variable. Make up the variable names and the number.
+
+c = 10
+d = 100 - c
+print (d)
+
+# Multiply a variable by itself and assign the result to the same variable. Make up the variable name.
+f = 150
+f = f * f
+print (f)
+
+# Multiply three numbers and assign the result to a variable. Make up the numbers and the variable name.
+
+g = 3 * 3 * 3
+
+
+# In one line of code, display the result of multiplying two variables. Make up the variable names.
+
+j = 10 
+k = 20 
+
+print (j * k)
+
+# Assign a number to a variable, and another number to a second variable. Multiply the two, using the variables. Assign the result to a third variable. Display the result using the third variable.  
+
+l = 1
+m = 2
+
+lm = l * m
+print(lm)
+
+# In a single line of code, divide one number by another and display the result.
+
+z = 150 / 2
+print(z)
