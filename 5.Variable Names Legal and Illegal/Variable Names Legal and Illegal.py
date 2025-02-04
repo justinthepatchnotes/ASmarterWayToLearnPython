566f5502f3d80f4fd3e26c27a3b28129664f4a1c
Justin J. <justinthepatchnotes@gmail.com>
2025-02-04 06:59:12 -0500

    Create Variable Names Legal and Illegal.py

---
+++ 5.Variable Names Legal and Illegal/Variable Names Legal and Illegal.py
@@ -0,0 +1,69 @@
+# Type the illegal character in this variable name:
+
+# 2nd_address
+
+2
+
+# Type the illegal character in this variable name:
+
+# fourth-Player  - 
+
+# Rewrite this variable name using the recommended style: customerlastname  
+
+customer_last_name = 'justin'
+
+# Rewrite this variable name using the recommended style:
+
+# totalaftertax
+
+total_after_tax = 1.25
+
+# Make up a variable name that includes a number.
+
+total_1 = 5
+
+
+# Assign a string to a variable. Make up the string and the variable name.
+
+random_string = 'random string'
+
+# Assign a number to a variable. Make up the number and the variable name.\
+
+random_num = 1.31
+
+# Assign the sum of two numbers to a variable. Make up the numbers and the variable name.
+
+sum_of_2 = 2 + 1
+print(sum_of_2)
+
+# Assign one variable to another. Make up the variable names.
+
+a = 1
+b = 1.43
+
+c = a + b
+print(c)
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+
+a = 1
+b = 1.43
+
+d = a / b
+
+4h="yes"
+negative answer = "no"
+asian_country = "China"
+african-country = "Kenya"
+print("ok")
+
+
+
+1stvariable = 1
+another-variable = 2
+onemorevariable = 3
+print(another-variable)
+
+
+
+
