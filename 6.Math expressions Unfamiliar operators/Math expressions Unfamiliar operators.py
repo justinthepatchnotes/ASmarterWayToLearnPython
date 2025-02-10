e8c2aecc9de885a37fc32b3ece027dc680f27ac3
Justin J. <justinthepatchnotes@gmail.com>
2025-02-09 21:17:24 -0500

    Create Math expressions Unfamiliar operators.py

---
+++ 6.Math expressions Unfamiliar operators/Math expressions Unfamiliar operators.py
@@ -0,0 +1,90 @@
+# Type the modulo operator
+
+a = 1 % 2
+
+print(a)
+
+
+# What is the name of this operator (one word, lowercase)? %
+
+# modulo 
+
+# Write the code that finds the remainder when 8 is divided by 3.
+
+b = 8 % 3
+
+print(b)
+
+# Find the remainder when you divide 12 by 3. Assign the result to the variable left_over.
+
+left_over = 12 % 3
+
+print (left_over)
+
+# Code the shorthand version of the following statement:
+
+total = 1
+
+total = total + 5
+
+total += 5
+
+print(total)
+
+# Code the shorthand version of the following statement:
+
+loss = -2.5
+
+loss = loss - 5
+
+loss -= 5
+
+print(loss)
+
+# Find the remainder when you divide one number by another and assign the result to a variable. Make up the numbers and the variable name.
+
+f = 10 % 5
+
+print(f)
+
+# Use shorthand code to multiply a variable by a number and assign the result to the same variable. Make up the variable name and the number.
+
+abc = 12
+
+abc *= 100
+
+print(abc)
+
+
+# Find what's left over when you divide one variable by another and assign the result to a third variable. Make up the variable name
+
+ac = 1
+
+cd = 25
+
+kl = ac % cd
+
+print(kl)
+
+# Using shorthand code, subtract one variable from another and assign the result to the first variable. Make up the variable names.
+
+kl -= cd
+
+# display the remainder—that is, what's left over—when 100 is divided by 11.
+
+left_over = 100 / 11
+print(left_over)
+
+# Rewrite lines 2 and 3 using the shorthand you used in this chapter.
+
+x = 100
+x = x + 3
+x = x / x
+
+print(x)
+
+x += 3
+print(x)
+
+x /= 3
+print(x)
